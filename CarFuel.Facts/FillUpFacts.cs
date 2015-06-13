8f23f2e728a532b007ca73de2a48cdddca41342f
your name <kanokwaaan@hotmail.com>
2015-06-13 15:53:50 +0700

    130615

--- CarFuel.Facts/FillUpFacts.cs
+++ CarFuel.Facts/FillUpFacts.cs
@@ -1,4 +1,5 @@
 ﻿using CarFuel.Models;
+using CarFuel.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,29 +16,108 @@ namespace CarFuel.Facts
             [Fact]
             public void Basic()
             {
-                //Arrange
+                // Arrange
                 var f1 = new FillUp();
                 f1.Odometer = 1000;
                 f1.IsFull = true;
                 f1.Liters = 50.0;
 
-                //Act
-                //Assert
+                // Act
+
+                // Assert
                 Assert.Equal(1000, f1.Odometer);
                 Assert.Equal(true, f1.IsFull);
-                Assert.Equal(50.5, f1.Liters);
+                Assert.Equal(50.0, f1.Liters);
 
             }
 
             [Fact]
             public void DefaultDateShouldBenow()
             {
-                var f = new FillUp();
+                // a
+                var now = DateTime.Now;
+                SystemTime.SetDateTime(now);
 
+                var f = new FillUp();
+                f.Odometer = 1000;
+                f.IsFull = true;
+
+                f.Liters = 50.0;
+
+                // a
                 DateTime dt = f.Date;
 
-                Assert.Equal(expected: DateTime.Now, actual: dt);
+                // a
+                Assert.Equal(expected: now, actual: dt);
+            }
+
+        }
+        public class KilometerPerLiterProperty
+        {
+
+            [Fact]
+            public void FirstFillUp_HasNoKmL()
+            {
+                // Arrange
+                var f1 = new FillUp();
+                f1.Odometer = 1000;
+                f1.IsFull = true;
+                f1.Liters = 50.0;
+
+                double? kml = f1.KilometerPerLiter;
+
+                Assert.Null(kml);
+            }
+
+            [Fact]
+            public void SecondFillUp()
+            {
+                // Arrange
+                var f1 = new FillUp();
+                f1.Odometer = 1000;
+                f1.Liters = 50.0;
+                f1.IsFull = true;
+
+                var f2 = new FillUp();
+                f2.Odometer = 1500;
+                f2.Liters = 40.0;
+                f2.IsFull = true;
+
+                f1.NextFillUp = f2;
+                double? kml = f1.KilometerPerLiter;
+
+                Assert.Equal(12.5, kml);
+            }
+
+            [Fact]
+            public void ThirdFillUp()
+            {
+                // Arrange
+                var f1 = new FillUp();
+                f1.Odometer = 1000;
+                f1.Liters = 50.0;
+                f1.IsFull = true;
+
+                var f2 = new FillUp();
+                f2.Odometer = 1500;
+                f2.Liters = 40.0;
+                f2.IsFull = true;
+
+                var f3 = new FillUp();
+                f3.Odometer = 2100;
+                f3.Liters = 50.0;
+                f3.IsFull = true;
+
+                f1.NextFillUp = f2;
+                f2.NextFillUp = f3;
+
+                double? kml = f2.KilometerPerLiter;
+
+                Assert.Equal(12.0, kml);
             }
         }
+
     }
+
+
 }
--- CarFuel.Models/FillUp.cs
+++ CarFuel.Models/FillUp.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarFuel.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,9 +17,25 @@ namespace CarFuel.Models
 
         public DateTime Date { get; set; }
 
+        public FillUp NextFillUp { get; set; }
+
         public FillUp()
         {
-            this.Date = DateTime.Now;
+            this.Date = SystemTime.Now();
+        }
+
+        public double? KilometerPerLiter
+        {
+            get
+            {
+                if (this.NextFillUp == null)
+                {
+                    return null;
+                }
+
+                return (this.NextFillUp.Odometer - this.Odometer)
+                       / this.NextFillUp.Liters;
+            }
         }
     }
 }
